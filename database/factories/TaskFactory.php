69b0b02c34ab31f18629732ce7ded586d5003704
Bahdanovich91 <113859381+Bahdanovich91@users.noreply.github.com>
2025-11-17 20:42:45 +0300

    Added tests, small fixes

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Enums\TaskStatus;
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+final class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title(),
+            'description' => $this->faker->text(13),
+            'status' => $this->faker->randomElement(TaskStatus::toArray()),
+        ];
+    }
+
+    public function pending(): self
+    {
+        return $this->state(fn() => ['status' => TaskStatus::PENDING->value]);
+    }
+}
